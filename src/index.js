0a662df0dfead1fe166b0999eb4af7e97a1971ad
Dick Törnfeldt <dicktornfeldt@Dicks-MacBook-Pro.local>
2018-10-05 08:29:06 +0200

    unregister serviceworkse

--- src/index.js
+++ src/index.js
@@ -32,3 +32,10 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// remove all current serviceworkers
+navigator.serviceWorker.getRegistrations().then(function(registrations) {
+  for (let registration of registrations) {
+    registration.unregister();
+  }
+});
