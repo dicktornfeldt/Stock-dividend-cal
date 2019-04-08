2d577c06e79ebfc44329b354fbeb15a051e2f86c
Dick Törnfeldt <dicktornfeldt@Dicks-MBP.lan>
2019-04-08 19:31:33 +0200

    added sek and percent to pie charts

--- src/components/Main/PieStocks.js
+++ src/components/Main/PieStocks.js
@@ -46,6 +46,21 @@ class PieStocks extends React.PureComponent {
 
       options = {
         legend: { display: false },
+        tooltips: {
+          callbacks: {
+            label: function(tooltipItem, data) {
+              var dataset = data.datasets[tooltipItem.datasetIndex];
+              var meta = dataset._meta[Object.keys(dataset._meta)[0]];
+              var total = meta.total;
+              var currentValue = dataset.data[tooltipItem.index];
+              var percentage = parseFloat(((currentValue / total) * 100).toFixed(1));
+              return currentValue + ':- (' + percentage + '%)';
+            },
+            title: function(tooltipItem, data) {
+              return data.labels[tooltipItem[0].index];
+            },
+          },
+        },
       };
     }
 
